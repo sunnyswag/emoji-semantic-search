b919b97866169c147bf9532219bc233c7c8cc771
sunnyswag <hhq126152@gmail.com>
2023-05-31 08:46:35 +0800

    add saveToClipboard func

--- client/Android/app/src/main/java/com/example/emojisemanticsearch/MainActivity.kt
+++ client/Android/app/src/main/java/com/example/emojisemanticsearch/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.emojisemanticsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.emojisemanticsearch.ui.theme.EmojiSemanticSearchTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +35,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.clickable {
+            scope.launch {
+                saveToClipboard(context, "Hello $name!")
+            }
+        }
     )
 }
 
---
+++ client/Android/app/src/main/java/com/example/emojisemanticsearch/UtilsExt.kt
@@ -0,0 +1,15 @@
+package com.example.emojisemanticsearch
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
+
+/**
+ * save str to clipboard
+ */
+fun saveToClipboard(context: Context, str: String) {
+    val clipboard = context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+    val clip = ClipData.newPlainText("label", str)
+    clipboard.setPrimaryClip(clip)
+}
