adc2b4ee73c2976624ec90305276da262e3bc1a0
sunnyswag <hhq126152@gmail.com>
2023-06-01 08:08:30 +0800

    setSystemBarsColor adjust to current theme

--- client/Android/app/src/main/java/com/example/emojisemanticsearch/MainActivity.kt
+++ client/Android/app/src/main/java/com/example/emojisemanticsearch/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.emojisemanticsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,15 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.emojisemanticsearch.ui.theme.EmojiSemanticSearchTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,16 +34,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column (modifier = Modifier.fillMaxSize()) {
-                        val systemUiController = rememberSystemUiController()
-                        val useDarkIcons = MaterialTheme.colors.isLight
-
-                        SideEffect {
-                            systemUiController.setSystemBarsColor(
-                                color = Color.Transparent,
-                                darkIcons = useDarkIcons
-                            )
-                        }
-
                         SearchEmoji(modifier = Modifier.fillMaxWidth())
                     }
                 }
@@ -67,29 +53,6 @@ fun SearchEmoji(modifier: Modifier) {
         onValueChange = { searchText = it },
         modifier = modifier.padding(10.dp)
     )
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val scope = rememberCoroutineScope()
-    val context = LocalContext.current
-
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.clickable {
-            scope.launch {
-                saveToClipboard(context, "Hello $name!")
-            }
-        }
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EmojiSemanticSearchTheme {
-        Greeting("Android")
-    }
 }
 
 @Preview(showBackground = true)
--- client/Android/app/src/main/java/com/example/emojisemanticsearch/ui/theme/Theme.kt
+++ client/Android/app/src/main/java/com/example/emojisemanticsearch/ui/theme/Theme.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -67,4 +68,12 @@ fun EmojiSemanticSearchTheme(
         typography = Typography,
         content = content
     )
+
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = colorScheme.background
+        )
+    }
 }
