dd817c1cc8f6f78ca40005ce072b18fe744e52f2
sunnyswag <hhq126152@gmail.com>
2023-06-01 08:25:05 +0800

    add onSearch action

--- client/Android/app/src/main/java/com/example/emojisemanticsearch/MainActivity.kt
+++ client/Android/app/src/main/java/com/example/emojisemanticsearch/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.emojisemanticsearch
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,6 +21,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,12 +51,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SearchEmoji(modifier: Modifier) {
     var searchText by remember { mutableStateOf(TextFieldValue(""))}
+    val context = LocalContext.current
 
     TextField(
         value = searchText,
         placeholder = { Text("Find the most relevant emojis") },
         onValueChange = { searchText = it },
-        modifier = modifier.padding(10.dp)
+        modifier = modifier.padding(10.dp),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                Toast.makeText(
+                    context,
+                    "Search for ${searchText.text}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        )
     )
 }
 
